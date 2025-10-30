c89fc7af5b8a21d8a445aa9c2a2a8eb1662bdb49
dmiante <dmiantedev@gmail.com>
2025-10-29 23:30:51 -0300

    UPDATE footer styles and responsive

--- src/pages/Footer.svelte
+++ src/pages/Footer.svelte
@@ -4,20 +4,21 @@
 
 </script>
 <footer
-  class="w-full pt-3"
+  class="flex justify-between items-end text-white text-xl"
 >
-  <div class="my-10">
-    <ul class="flex justify-around items-center text-white text-xl">
-      <li>
-        <span class="text-white font-mono">
-        Made with
-          <a href="https://cloudinary.com" target="_blank" rel="noreferrer"
-          ><CloudinaryLogo /></a>
-        </span>
-      </li>
-      <li>
-        <span>Built by <a class="text-2xl font-mono underline underline-offset-4" href="https://github.com/dmiante" target="_blank" rel="noreferrer">Dmiante</a>🦁</span>
-      </li>
-    </ul>
-  </div>
+  <p class="text-white font-mono text-sm">
+  Made with
+    <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer"
+    ><CloudinaryLogo /></a>
+  </p>
+  <span class="text-sm">Built by 
+    <a 
+    class="text-2xl font-mono hover:underline hover:underline-offset-4 hover:text-fuchsia-600" 
+    href="https://github.com/dmiante" 
+    target="_blank" 
+    rel="noopener noreferrer">
+    Dmiante
+    </a>
+    🦁
+  </span>
 </footer>
