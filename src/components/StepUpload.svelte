d7656b2958880ac531485974f105edbd9cfea01e
dmiante <dmiantedev@gmail.com>
2025-10-29 23:30:24 -0300

    UPDATE upload form styles and responsive

--- src/components/StepUpload.svelte
+++ src/components/StepUpload.svelte
@@ -58,17 +58,23 @@
 
 <form
   id="dropzone"
-  class="shadow-2xl border-dashed border-2 border-[#4a044e] rounded-lg aspect-video w-2/5 flex items-center justify-center flex-col cursor-pointer hover:bg-[#4a044e]"
+  class="border-dashed border-2 border-[#4a044e] rounded-lg w-full flex items-center justify-center flex-col hover:bg-[#4a044e] py-10 lg:cursor-pointer lg:max-w-6xl"
   action="https://api.cloudinary.com/v1_1/midudev/image/upload"
 >
   {#if $imageStatus === ImageStatus.READY}
     <button
-      class="font-bold pointer-events-none bg-blue-800 rounded-full text-bold text-white text-xl px-6 py-4"
+      class="font-bold pointer-events-none bg-blue-800 rounded-full text-bold text-white text-xl px-6 py-4 flex items-center gap-2 hover:bg-blue-600 transition"
     >
-      Upload files
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-upload-icon lucide-upload"><path d="M12 3v12"/><path d="m17 8-5-5-5 5"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/></svg>
+      Upload photo
     </button>
-    <strong class="text-lg mt-4 text-gray-400">or drop a file</strong>
+    <strong class="text-lg mt-4 text-gray-400 text-center">
+      Drag & drop your image here, or click to select files
+    </strong>
   {:else if $imageStatus === ImageStatus.UPLOADING}
-    <strong class="text-white text-lg mt-4">Uploading files...</strong>
+    <strong class="text-white text-lg flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-loader-circle-icon lucide-loader-circle animate-spin"><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>
+      Uploading files...
+    </strong>
   {/if}
 </form>
