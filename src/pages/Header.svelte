5d21696aaa0b413b9f1ebf7c7023c6e63d78c9ae
dmiante <dmiantedev@gmail.com>
2025-10-29 23:31:10 -0300

    UPDATE header styles and responsive

--- src/pages/Header.svelte
+++ src/pages/Header.svelte
@@ -1,5 +1,5 @@
-<header class="flex flex-col text-center my-5">
-  <a href="/">
+<header class="flex flex-col text-center">
+  <a href="/" rel="noopener noreferrer">
     <h1 class="text-7xl font-bold text-fuchsia-600 tracking-tighter">
       Remove<span class="text-blue-500">BG</span>
     </h1>
